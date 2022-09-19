a3793a8485d4fe3f5527400797a595ce3168c406
Intan Rashidah <intanrashidah97@gmail.com>
2022-09-19 16:45:16 +0800

    Initial and Final Commit

---
+++ mysite/students/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import*
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name","email_id"]
+
+
+@admin.register(Departments)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ["department_name"]
---
+++ mysite/students/views.py
@@ -0,0 +1,16 @@
+from types import NoneType
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+# Create your views here.
+def home(request):
+    students = Student.objects.all()
+    return render(request,'home.html',{'students':students})
+
+
+def student(request,student_id):
+    try:
+        student = Student.objects.get(id=student_id)
+    except Student.DoesNotExist:
+        student = None
+    return render(request,'student.html',{'student':student})
